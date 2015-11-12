fc7e528e9fe600953c340625fa0aaca3f4fc8884
ChGreb <Christian.Greb@hhu.de>
2015-11-12 10:41:16 +0100

    hw3 done

--- statistics.cxx
+++ statistics.cxx
@@ -4,16 +4,35 @@
 
 using namespace std;
 //--------------------------------------------------
-// plenty of lines here
+// Homework3	Statisitcs
+// Christian Greb	2030036
 //--------------------------------------------------
+
+void RandNum(double* x, int N){
+  for(int i=0; i<N; i++)
+    x[i] = rand()/double(RAND_MAX);
+}
+
+void stat(double* p, double& mean, double& var, int N){
+  
+  mean = 0;
+    for(int i=0; i<N; i++)	//Sum all random values and calc the mean value
+      mean += p[i]/N;
+    
+  var = 0;
+    for(int i=0; i<N; i++)	//Sum over all "displacements" and calc the variance
+      var = ((p[i]-mean)*(p[i]-mean))/N;
+}
+
 
 int main(){
    const int N = 100;
    double p[N];
    double mean, var;
 
-   // Some lines here....
-
+  RandNum(p, N);
+  stat(p, mean, var, N);
+   
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
 
